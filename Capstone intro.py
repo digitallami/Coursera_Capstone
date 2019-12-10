5eadad52809e7f57030994a8ec616216b2868c96
digitallami <37887215+digitallami@users.noreply.github.com>
2019-12-10 16:35:38 +0100

    Add files via upload

---
+++ Capstone intro.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # THIS NOTEBOOK WILL BE MAINLY USED FOR THE COURSERA CAPSTONE PROJECT
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
